1a20f6ad51f4b04c5d9d8f833eeb12c5baeeab84
gashnick <gashnicolas@gmail.com>
2024-10-10 20:45:50 +0200

    conect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then((res) => {
+    console.log("connect to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
