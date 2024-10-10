24ed75ad470a6bf3ea48e2b21696ed14d4aa24ef
gashnick <gashnicolas@gmail.com>
2024-10-10 19:15:23 +0200

    create header file

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
-import About from "./pages/about";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
-        <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sign-up" element={<Signup />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: 'url("/images/image1.jpg")', // Adjust the path to your image
+        backgroundSize: "cover", // Ensures the image covers the entire header
+        backgroundPosition: "center", // Centers the background image
+        height: "100vh",
+        width: "100%",
+        display: "flex", // Flexbox to position content if needed
+        justifyContent: "center", // Center content horizontally
+        alignItems: "center", // Center content vertically
+      }}
+    >
+      <h1 className="font-bold text-white">LIBRARY MANAGEMENT SYSTEM</h1>
+    </div>
+  );
 }
